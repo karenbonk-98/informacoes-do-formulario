2e5dc92d4ef0de8a7dfbc360161362c2e49252ea
karen pereira candido <60606971+karenbonk-98@users.noreply.github.com>
2023-11-25 13:23:00 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require('header.php');
+?>
+
+
+<form method="GET" action="recebedor.php">
+
+    <label>
+        Nome:
+        <br/>
+        <input type="text" name="nome" />
+    </label>
+    </br>
+    </br>
+
+    <label>
+        Idade:
+        <br/>
+        <input type="text" name="idade" />
+    </label>
+    </br>
+    </br>
+
+    <input type="submit" value="Enviar" />
+
+</form>
